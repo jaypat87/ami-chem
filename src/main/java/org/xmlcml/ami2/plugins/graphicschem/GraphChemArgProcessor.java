c44fde073be504dbcd07953a23e6da01692cdb86
AndyHowlettGitHub <andyhowlett@googlemail.com>
2016-08-11 16:26:20 +0100

    Switched code to use new class CTree. Tidied some debugging strings. Tidied some documentation. Tidied some comments. Formatting changes.

--- src/main/java/org/xmlcml/ami2/plugins/graphicschem/GraphChemArgProcessor.java
+++ src/main/java/org/xmlcml/ami2/plugins/graphicschem/GraphChemArgProcessor.java
@@ -1,19 +1,20 @@
 package org.xmlcml.ami2.plugins.graphicschem;
 
 import java.io.File;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.xmlcml.ami2.chem.ChemistryBuilder;
 import org.xmlcml.ami2.chem.MoleculeCreator;
+import org.xmlcml.ami2.chem.svg.SVGContainerNew;
 import org.xmlcml.ami2.plugins.AMIArgProcessor;
 import org.xmlcml.cmine.args.ArgIterator;
 import org.xmlcml.cmine.args.ArgumentOption;
-import org.xmlcml.cmine.files.CMDir;
+import org.xmlcml.cmine.files.CTree;
 import org.xmlcml.cmine.files.ResultElement;
 import org.xmlcml.cmine.files.ResultsElement;
 import org.xmlcml.cml.element.CMLMolecule;
@@ -21,7 +22,7 @@ import org.xmlcml.graphics.svg.SVGElement;
 import org.xmlcml.graphics.svg.SVGUtil;
 
 /** 
- * Processes commandline arguments.
+ * Processes command-line arguments.
  * 
  * @author pm286
  */
@@ -31,11 +32,12 @@ public class GraphChemArgProcessor extends AMIArgProcessor {
 	private List<String> params;
 	private SVGElement inputSvg;
 	private CMLMolecule molecule;
+	
 	static {
 		LOG.setLevel(Level.DEBUG);
 	}
 
-	// shouldn't be required // fails to inherit on Jenkins
+	//Shouldn't be required; fails to be inherited on Jenkins
 	private static String WHITESPACE = "\\s+";
 	
 	public GraphChemArgProcessor() {
@@ -51,21 +53,21 @@ public class GraphChemArgProcessor extends AMIArgProcessor {
 		this(argString.split(WHITESPACE));
 	}
 
-	// =============== METHODS ==============
+	//=============== METHODS ==============
 
 	public void parseChem(ArgumentOption option, ArgIterator argIterator) {
 		params = argIterator.createTokenListUpToNextNonDigitMinus(option);
-		LOG.debug("parseChem args "+params);
+		LOG.debug("After parsing, arguments: " + params);
 	}
 	
 	public void runChem(ArgumentOption option) {
-		File svgFile = currentCMDir.getExistingFileWithReservedParentDirectory(inputList.get(0));
-		LOG.trace("SVG File: "+svgFile + "/"+ inputList.get(0));
+		File svgFile = getCTree().getExistingFileWithReservedParentDirectory(inputList.get(0));
+		LOG.trace("SVG File: " + svgFile + "/" + inputList.get(0));
 		inputSvg = null;
 		try {
 			inputSvg = SVGUtil.parseToSVGElement(new FileInputStream(svgFile));
 		} catch (FileNotFoundException e) {
-			throw new RuntimeException("Cannot read SVG file: "+svgFile, e);
+			throw new RuntimeException("Cannot read SVG file: " + svgFile, e);
 		}
 		ChemistryBuilder geometryBuilder = new ChemistryBuilder(inputSvg);
 		MoleculeCreator moleculeCreator = new MoleculeCreator(geometryBuilder);
@@ -73,35 +75,34 @@ public class GraphChemArgProcessor extends AMIArgProcessor {
 	}
 
 	public void outputChem(ArgumentOption option) {
-		LOG.debug("outputChem "+option);
+		LOG.debug("Before outputting, option: " + option);
 		String outputFilename = getOutput();
-		if (!CMDir.isReservedFilename(outputFilename)) {
-//			throw new RuntimeException("Output is not a reserved file: "+outputFilename);
-			LOG.error("Output is not checked as reserved file: "+outputFilename);
+		if (!CTree.isReservedFilename(outputFilename)) {
+			//throw new RuntimeException("Output is not a reserved file: "+outputFilename);
+			LOG.error("Output is not checked as reserved file: " + outputFilename);
 		}
 		ResultsElement resultsElement = new ResultsElement();
 		ResultElement resultElement = new ResultElement();
-		// meaningless - replace by chemistry
+		//Meaningless - replace by chemistry
 		
 		resultElement.appendChild(molecule);
 		resultsElement.appendChild(resultElement);
-		File outputFile = new File(currentCMDir.getDirectory(), outputFilename);
+		File outputFile = new File(getCTree().getDirectory(), outputFilename);
 		outputFile.getParentFile().mkdirs();
-		LOG.debug("mkdir: "+outputFile+"; "+outputFile.exists()+"; "+outputFilename);
+		LOG.debug("mkdir: " + outputFile+"; " + outputFile.exists() + "; " + outputFilename);
 		getOrCreateContentProcessor().writeResults(outputFile, resultsElement);
 	}
 	
-	// =============================
+	//=============================
 
 	@Override
-	/** parse args and resolve their dependencies.
+	/** 
+	 * Parse arguments and resolve their dependencies.
 	 * 
-	 * (don't run any argument actions)
-	 * 
+	 * (Don't run any argument actions.)
 	 */
 	public void parseArgs(String[] args) {
 		super.parseArgs(args);
 	}
-
 	
 }
